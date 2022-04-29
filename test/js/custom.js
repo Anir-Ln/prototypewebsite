8cf35c87fa959dd326688c2e4793eb05d123311e
Mohamed Lahyane <71042937+Anir-Ln@users.noreply.github.com>
2022-04-29 00:17:57 +0000

    test website

---
+++ test/js/custom.js
@@ -0,0 +1,6 @@
+let chat_box = document.getElementById('chat-circle');
+chat_box.addEventListener('click', () => {
+    let url = `https://wa.me/+212621190278?text=`;
+    window.open(url, '_blank');
+})
+
